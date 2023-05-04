3d309daa38ae26fa9a308e28e1da9d2798b18aa3
abdullah-aljoulani <abdullahaljoulani@gmail.com>
2023-05-04 08:51:16 +0000

    creating movie app

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# movies-library
+# Project Name - movies-library
+
+**Author Name**: Abdullah Aljoulani
+
+## WRRC
+
+?[app-img](./Untitled.jpg)
+
+## Overview
+
+## Getting Started
+
+first should be import the express frameWork & cors , then you should store the all method then create a constructor function  to ensure your data follow the same format., after that you need to give a PORT to localhost and define the home route, then if you have a lot of pages you need to define them also .
+
+## Project Features
+
+-show the list of movies -search about specific movie -you can see what's your favorite Movies
---
+++ server.js
@@ -0,0 +1,48 @@
+`use strict`
+
+const express = require('express');
+const cors = require("cors");
+
+const app = express()
+
+app.use(cors())
+
+
+
+
+app.listen(3000 , () => console.log("Hello from movie library"))
+
+
+function Movies(title, poster_path, overview) {
+    this.title = title
+    this.poster_path = poster_path
+    this.overview = overview
+}
+
+
+
+app.get("/", (req, res) => {
+    const data = require("./data.json");
+    let newMovie = new Movies(
+        data.title , data.poster_path ,data.overview
+    )
+    res.send(newMovie)
+})
+
+
+app.get("/favorite",(req,res)=>{
+    let str = "Welcome to Favorite Page";
+    res.status(200).send(str);
+
+})
+
+app.get("*",(req,res)=>{
+    let str3= "page not found error"
+        res.status(404).send(str3)
+    })
+
+
+app.get("*",(req,res)=>{
+let str2= "Sorry, something went wrong"
+    res.status(500).send(str2)
+})
